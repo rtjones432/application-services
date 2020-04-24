0a379fe5683874adc0b04d443f25bf74248ca390
Hannah <computernerd510@protonmail.com>
2020-04-24 15:28:35 -0500

    tests failing

---
+++ clobber_mechanism/src/bin/lib.rs
@@ -0,0 +1,150 @@
+/*
+* This is a script to clobber the {desktop, ios, android} directories
+* and rebuild libs via the build-all.sh script.
+*/
+use std::fs;
+use std::cmp::max;
+use std::path::{Path, PathBuf};
+use std::env;
+use std::process::Command;
+use std::io::prelude::*;
+use structopt::StructOpt;
+
+#[allow(non_snake_case)]
+pub enum Status {
+    OK,
+    FAIL,
+}
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "clobber", about = "eeeeeeeeeeeeeeeeeeeeeee")]
+struct Opt {
+    #[structopt(short = "s",
+    long  = "script",
+    help  = "script to rebuild the library",
+    default_value = "build-all.sh")]
+    script: String,
+
+    #[structopt(short = "d",
+    long  = "directories",
+    parse(from_os_str),
+    default_value = "desktop ios android")]
+    #[structopt(help = "Input files or directories",
+    parse(from_os_str))]
+    inputs: Vec<PathBuf>
+}
+
+
+// function reads the most recent version number. (everything before the '.')
+fn parse(file: String) -> i32 {
+
+    let mut most_recent_version = -1;
+    for line in file.lines() {
+        let v: Vec<&str> = line.split('.').collect();
+        if v.len() != 2 {
+            continue;
+        }
+        if let Ok(v) = v[0].parse::<i32>() {
+            most_recent_version = max(v, most_recent_version);
+        }
+    }
+    most_recent_version
+}
+#[allow(unused_must_use)]
+fn parse_version(newest_version: String, local_version: String) -> Result<bool, String> {
+
+    if parse(newest_version.clone()) != parse(local_version) {
+        return Ok(true)
+    }
+    Ok(false)
+}
+
+#[allow(unused_must_use)]
+fn run_dependency_check(vl: String, loc_version: String) -> Result<bool, String> {
+
+    let root = Path::new("../../libs");
+    assert!(env::set_current_dir(&root).is_ok());
+    //  If there is no local version log, clobber is needed.
+    if !(Path::new(&loc_version).exists()) {
+        fs::File::create(".local_libs_version");
+        return Ok(true);
+    }
+
+    let mut version_log = fs::File::open(vl).expect("Error: version_log is missing. :(\n");
+    let mut newest_version = String::new();
+    version_log.read_to_string(&mut newest_version);
+
+    let mut local_file = fs::File::open(loc_version).expect("damn.");
+    let mut local_version = String::new();
+    local_file.read_to_string(&mut local_version);
+
+    return parse_version(newest_version, local_version);
+    //fs::write("../libs/.local_libs_version",  )
+}
+
+
+fn clobber(directories: &mut Vec<String>) -> Status {
+
+    println!("deleting old directories and rebuilding /libs...\n");
+
+    for x in directories {
+        println!("{}", x);
+        let curr_dir = Path::new(&x);
+        if curr_dir.exists() {
+            match fs::remove_dir_all(curr_dir) {
+                Ok(_t) => {},
+                Err(_e) => return Status::FAIL,
+            }
+        }
+    }
+    Status::OK
+}
+
+
+fn rebuild(build_script: String) -> Status {
+
+    // Now execute the build-all script in a shell.
+    let mut cmd = Command::new("bash");
+    cmd.arg(build_script);
+    match cmd.output() {
+        Ok(_t) => {},
+        Err(_e) => return Status::FAIL,
+    };
+    Status::OK
+}
+
+fn run_process(script: String, mut dirs_to_clobber: Vec<String>) {
+
+    let version_log = String::from("version_log");
+    let local_version = String::from(".local_libs_version");
+
+    let clobber_needed: bool;
+    clobber_needed = run_dependency_check(version_log.clone(), local_version.clone()).unwrap();
+
+    println!("clobbering: {:?}", dirs_to_clobber);
+    if clobber_needed {
+        match clobber(&mut dirs_to_clobber) {
+            Status::OK => println!("Directories successfully clobbered!\n"),
+            Status::FAIL => println!("Counld not rebuild libs. Delete your folders for desktop, ios, and android, then manually run libs/build-all.sh \n"),
+        }
+        rebuild(script);
+    }
+    //update the local version log.
+    match fs::copy(&version_log, &local_version) {
+        Ok(_) => {},
+        Err(_) => {},
+    }
+}
+
+#[allow(dead_code)]
+fn main() {
+    let opt = Opt::from_args();
+    println!("{:#?}", opt);
+    let script = String::from("build-all.sh");
+    let dirs_to_clobber = vec![String::from("desktop"), String::from("ios"), String::from("android")];
+    run_process(script  , dirs_to_clobber);
+
+}
+
+#[cfg(test)]
+mod tests;
---
+++ clobber_mechanism/src/bin/tests.rs
@@ -0,0 +1,71 @@
+use super::*;
+use std::char::decode_utf16;
+use std::fs::{self, File};
+use std::io::Write;
+use tempdir::TempDir;
+
+
+#[test]
+fn test_parse() {
+
+    let mut tempVersion = String::from("0. Initial version.");
+    let mut tempVersionLog = String::from("2. more changes \n 0. Initial version.\n 1. SomeChanges");
+    assert_eq!(parse_version(tempVersionLog, tempVersion), Ok(true));
+}
+
+#[test]
+fn test_run_dependency_check() {
+
+}
+
+#[test]
+fn test_clobber() {
+
+    //let root = Path::new("../../libs");
+    //assert!(env::set_current_dir(&root).is_ok());
+
+    let s = Status::OK;
+    let dir1 = TempDir::new_in(".", "test1");
+    let dir2 = TempDir::new_in(".", "test2");
+    let dir3 = TempDir::new_in(".", "test3");
+    let mut test_dir_list = vec![String::from("test1"), String::from("test2"), String::from("test3")];
+    let mut tdl = vec![dir1, dir2, dir3];
+
+    // Check that the temp directory actually exists.
+    let mut tmp_path;
+    for x in &tdl {
+        tmp_path = x.as_ref().unwrap().path().to_owned();
+        assert!(tmp_path.exists());
+    }
+
+    clobber(&mut test_dir_list);
+
+    //make sure directories no longer exist
+    for x in &tdl {
+        tmp_path = x.as_ref().unwrap().path().to_owned();
+        assert_eq!(tmp_path.exists(), false);
+    }
+}
+
+
+#[test]
+// run process with defaults
+fn end_to_end_test_1() {
+    /*
+    let opt = Opt::from_args();
+    let s = opt.script;
+    run_process(opt.script, opt.inputs); //this currently does what its supposed to...not with temp directories.
+    //assert_eq!();
+    */
+}
+
+#[test]
+// run proccess with custom commands.
+fn end_to_end_test_2() {
+    /*
+    let opt = Opt::from_args();
+    run_process(opt.script, opt.inputs);
+    //assert_eq!();
+    */
+}
+
